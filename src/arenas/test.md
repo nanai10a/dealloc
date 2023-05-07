cf5715d883570046c13faf3e9c73130e21600260
Nanai Jua <me@n7i.dev>
2023-05-08 03:00:44 +0900

    feat: add basic arena page

---
+++ src/arenas/test.md
@@ -0,0 +1,8 @@
+---
+ident: こんにちは
+traits: ["trait-01", "trait-02", "trait-03"]
+captured: 2023-05-08
+patched: 2023-05-08
+---
+
+つまりはそういうことです.
---
+++ src/pages/arenas/{file.name}.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { graphql } from "gatsby";
+
+type Props = {
+  data: {
+    markdownRemark: {
+      html: string;
+      frontmatter: {
+        ident: string;
+        traits: string[];
+        captured: string;
+        patched: string;
+      };
+    };
+  };
+};
+
+export default function ({ data }: Props) {
+  const { html, frontmatter } = data.markdownRemark;
+
+  return (
+    <div>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  );
+}
+
+export const pageQuery = graphql`
+  query ($id: String!) {
+    markdownRemark(parent: { id: { eq: $id } }) {
+      html
+      frontmatter {
+        ident
+        traits
+        captured(formatString: "YYYY-MM-DD")
+        patched(formatString: "YYYY-MM-DD")
+      }
+    }
+  }
+`;
