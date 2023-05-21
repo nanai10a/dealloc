d63fcf2148d8cd78aa108edd614c97d97066d9e5
Nanai Jua <me@n7i.dev>
2023-05-22 06:40:38 +0900

    refactor: use types provided by gatsby

--- src/pages/arenas/{file.name}.tsx
+++ src/pages/arenas/{file.name}.tsx
@@ -1,9 +1,10 @@
+import type { HeadProps, PageProps } from "gatsby";
 import Layout from "../../components/Layout";
 import React from "react";
 
 import { graphql } from "gatsby";
 export const pageQuery = graphql`
-  query ($id: String!) {
+  query Arenas($id: String!) {
     markdownRemark(parent: { id: { eq: $id } }) {
       html
       frontmatter {
@@ -16,35 +17,31 @@ export const pageQuery = graphql`
   }
 `;
 
-export type Props = {
-  data: {
-    markdownRemark: {
-      html: string;
-      frontmatter: {
-        ident: string;
-        traits: string[];
-        captured: string;
-        patched: string;
-      };
-    };
-  };
-};
+export function Head({ data }: HeadProps<Queries.ArenasQuery>) {
+  return (
+    <>
+      <title>{data.markdownRemark?.frontmatter?.ident} - dealloc</title>
+    </>
+  );
+}
 
-export default function ({ data }: Props) {
-  const {
-    html,
-    frontmatter: { ident, traits, captured, patched },
-  } = data.markdownRemark;
+export default function ({ data }: PageProps<Queries.ArenasQuery>) {
+  const { html, frontmatter } = data.markdownRemark ?? {};
+  const { ident, traits, captured, patched } = frontmatter ?? {};
 
   return (
     <Layout>
       <div className="max-w-4xl p-8 mx-auto border-2 border-stone-100 rounded-2xl">
         <div className="mb-4">
           <h1 className="text-4xl bold">{ident}</h1>
-          <p>{traits.join(", ")}</p>
+          <p>{traits?.join(", ")}</p>
           <p>{"captured: " + captured + ", patched: " + (patched ?? "no")}</p>
         </div>
-        <div className="arena" dangerouslySetInnerHTML={{ __html: html }} />
+
+        <div
+          className="arena"
+          dangerouslySetInnerHTML={{ __html: html ?? "" }}
+        />
       </div>
     </Layout>
   );
