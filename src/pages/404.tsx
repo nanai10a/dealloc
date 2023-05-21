b4edb88ba2c58fed9f6f5fc23fd0300408d639c4
Nanai Jua <me@n7i.dev>
2023-05-22 06:41:02 +0900

    feat: rewrite `/404`

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,32 @@
-import { Link, HeadFC, PageProps } from "gatsby"
+import type { HeadProps, PageProps } from "gatsby";
+import Layout from "../components/Layout";
 import React from "react";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+const texts = [
+  "in a wall...",
+  "falling at sky!",
+  "time is s l o w i  n  g",
+  "`void {u}` ed!",
+  "segmentation fault",
+  "broken route",
+  "",
+];
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-const NotFoundPage: React.FC<PageProps> = () => {
+export default function ({}: PageProps<{}>) {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
+    <Layout>
+      <h1 className="text-6xl">
+        {texts[Math.floor(Math.random() * texts.length)]}
+      </h1>
+      <p>status: 404</p>
+    </Layout>
+  );
 }
 
-export default NotFoundPage
-
-export const Head: HeadFC = () => <title>Not found</title>
+export function Head({}: HeadProps<{}>) {
+  return (
+    <>
+      <title>Not found - dealloc</title>
+    </>
+  );
+}
