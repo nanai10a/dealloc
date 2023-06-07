88b70c6a076cd88e7adcb9f389e579f8c6e0c441
Nanai Jua <me@n7i.dev>
2023-06-07 20:01:35 +0900

    feat: remove texts of `/404`

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,23 +2,10 @@ import type { HeadProps, PageProps } from "gatsby";
 import Layout from "../components/Layout";
 import React from "react";
 
-const texts = [
-  "in a wall...",
-  "falling at sky!",
-  "time is s l o w i  n  g",
-  "`void {u}` ed!",
-  "segmentation fault",
-  "broken route",
-  "",
-];
-
 export default function ({ location }: PageProps<{}>) {
   return (
     <Layout location={location}>
-      <h1 className="text-6xl">
-        {texts[Math.floor(Math.random() * texts.length)]}
-      </h1>
-      <p>status: 404</p>
+      <p className="mx-auto">status: 404</p>
     </Layout>
   );
 }
